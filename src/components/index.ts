6687b9f57f751899cfdb2d842287831734e26ae2
Elijah Boston <e.b.boston@gmail.com>
2019-11-06 15:42:06 -0500

    Export components used by pages

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export {default as Button} from './button'
+export {default as Layout} from './layout'
+export {default as Question} from './question'
+export {default as ResultList} from './result-list'
