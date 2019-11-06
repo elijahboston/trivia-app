26bdfa4eec092347112ef062df63bd743dfe65e0
Elijah Boston <e.b.boston@gmail.com>
2019-11-06 15:44:54 -0500

    Custom state-based router

---
+++ src/router.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import {useSelector} from 'react-redux'
+import {SplashPage, QuestionPage, ResultPage} from './pages'
+import {AppStateType} from './redux/types'
+
+/**
+ * A simple state based router component.
+ * @param props The game state to use for routing.
+ */
+const Router = () => {
+  const gameState = useSelector((state: AppStateType) => {
+    const { isLoading, isGameStarted, isGameOver } = state;
+    return {
+      isLoading,
+      isGameStarted,
+      isGameOver
+    }
+  });
+
+  if (gameState.isLoading) return <div>Loading...</div>
+
+  if (!gameState.isGameStarted) return <SplashPage />
+
+  if (gameState.isGameStarted) return <QuestionPage />
+
+  if (gameState.isGameOver) return <ResultPage />
+}
+
+export default Router;
