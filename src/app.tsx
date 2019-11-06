d4bdbf5057d9db27005f174a004a7a15222ad541
Elijah Boston <e.b.boston@gmail.com>
2019-11-06 15:46:08 -0500

    Setup store and routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,18 @@
-import * as React from 'react';
+import * as React from 'react'
+import {Provider} from 'react-redux'
+import store from './redux/store'
+import Router from './router'
 
-const App = () => {
-  return (
-    <div className='app'>
-      Hello world.
-    </div>
-  );
-}
-
+const App = () =>
+  <Provider store={store}>
+    <Router />
+    <style jsx global>{`
+      html {
+        margin: 0;
+        padding: 0;
+        font-family: 'Open Sans', sans-serif;
+      }
+    `}</style>
+  </Provider>
     
 export default App;
