89067f6a5101c36ada1b9a6c2101bbe8f721d63b
Elijah Boston <e.b.boston@gmail.com>
2019-11-07 14:33:49 -0500

    Add Loader component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {useSelector} from 'react-redux'
 import {AppState} from '../redux/types'
+import Icon from './icon'
 
 /**
  * This component is always on the page, just not always visible
@@ -9,20 +10,48 @@ const Loading = () => {
   const isLoading = useSelector((state: AppState) => state.api.isLoading)
 
   return (
-    <div className='loading'>
-      <h2>Loading</h2>
+    <div className='loading-container'>
+      <div className='loading'>
+        <h2>Loading</h2>
+        <div className='loading-icon'>
+          <Icon name='settings_backup_restore' fontSize='2rem' />
+        </div>
+      </div>
       <style jsx>{`
-        opacity: ${isLoading ? 1 : 0};
-        display: ${isLoading ? 'block' : 'none'};
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        transition: opacity .2s ease-in;
-        background: rgba(0, 0, 0, .5);
-        backdrop-filter: blur(2px);
+        @keyframes rotation {
+          0% { transform: rotate(0); }
+          100% { transform: rotate(-360deg); }
+        }
+
+        .loading-container {
+          position: absolute;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+
+          opacity: ${isLoading ? 1 : 0};
+          display: ${isLoading ? 'block' : 'none'};
+          transition: opacity .2s ease-in;
+          background: rgba(0, 0, 0, .5);
+          backdrop-filter: blur(2px);
+        }
+
+        .loading {
+          text-align: center;
+        }
+
+        .loading-icon {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          animation: rotation 2s infinite linear;
+          transform-origin: center center;
+        }
       `}</style>
     </div>
   )
 }
 
-export default Loading
+export default Loading;
