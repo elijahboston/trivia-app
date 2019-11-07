90a35700fe0308bf39001cacf38775243b6db232
Elijah Boston <e.b.boston@gmail.com>
2019-11-07 01:31:22 -0500

    Include global style and layout

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,17 @@
-import { configure } from '@storybook/react';
+import * as React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import {Layout} from '../src/components'
 
-configure(require.context('../src', true, /\.stories\.tsx$/), module);
+addDecorator(storyFn =>
+  <Layout>
+    {storyFn()}
+    <style jsx global>{`
+      html {
+        font-family: 'Open Sans', sans-serif;
+        margin: 1rem;
+        padding: 0;
+      }
+    `}</style>
+  </Layout>)
+
+configure(require.context('../src', true, /\.stories\.tsx$/), module)
